4c65407e267ef4fb94c34781317aebff34c2e452
Karl Tryggvason <ktryggvason@gmail.com>
2014-04-06 14:30:13 +0100

    Adding user agent information for Discogs api calls

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.use(express.static(__dirname + '/static'));
 app.get('/discogs', function(req,res) {
 	discogsurl = url.parse(req.url, true).query["url"];
 	if (discogsurl.indexOf("http://api.discogs.com") === 0){
-		request(discogsurl).pipe(res);
+		var options = {
+			url: discogsurl,
+			headers: {
+				'User-Agent': 'Disconest/1.0 +http://www.disconest.com'
+			}
+		};
+		request(options).pipe(res);
 	}else{
 		res.send(404, 'Sorry cant find that!');
 	}
@@ -17,7 +23,13 @@ app.get('/discogs', function(req,res) {
 app.get('/pixogs', function(req,res) {
 	discogsimg = url.parse(req.url, true).query["img"];
 	if (discogsimg.indexOf("http://api.discogs.com/image/")===0 && discogsimg.substring(discogsimg.length-4) == "jpeg"){
-		request(discogsimg).pipe(res);
+		var options = {
+			url: discogsimg,
+			headers: {
+				'User-Agent': 'Disconest/1.0 +http://www.disconest.com'
+			}
+		};
+		request(options).pipe(res);
 	}else{
 		res.send(404, 'Sorry cant find that!');
 	}
