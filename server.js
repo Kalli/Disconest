873c11c3bd38312943e9ae4a87f314cd32f2d258
Karl Tryggvason <ktryggvason@gmail.com>
2014-04-14 11:55:53 +0100

    Adding fix for image proxying

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app = express();
 app.use(express.static(__dirname + '/static'));
 
 app.get('/discogs', function(req,res) {
-	discogsurl = url.parse(req.url, true).query["url"];
+	var discogsurl = url.parse(req.url, true).query["url"];
 	if (discogsurl.indexOf("http://api.discogs.com") === 0){
 		var options = {
 			url: discogsurl,
@@ -21,8 +21,9 @@ app.get('/discogs', function(req,res) {
 });
 
 app.get('/pixogs', function(req,res) {
-	discogsimg = url.parse(req.url, true).query["img"];
-	if (discogsimg.indexOf("http://api.discogs.com/image/")===0 && discogsimg.substring(discogsimg.length-4) == "jpeg"){
+	var discogsimg = url.parse(req.url, true).query["img"];
+	if (discogsimg.indexOf("http://api.discogs.com/image/")===0 && (discogsimg.substring(discogsimg.length-4) == "jpeg" || discogsimg.substring(discogsimg.length-3) == "png" )){
+		discogsimg = discogsimg.replace("api.discogs.com","s.pixogs.com");
 		var options = {
 			url: discogsimg,
 			headers: {
