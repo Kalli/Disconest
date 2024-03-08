4d190d5cf66e1753c3fb1b22abc03c8431e20977
Karl Tryggvason <ktryggvason@gmail.com>
2024-03-08 15:10:22 +0200

    Add Discogs release fetching api

--- disconest/pages/api/discogs.ts
+++ disconest/pages/api/discogs.ts
@@ -1,18 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { makeRequest } from '../requests';
+
 type ResponseData = Object
 
-// performs a discogs search for a query
+// makes the relevant requests to the discogs api based on query parameters
 export default async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
-    let discogsSearchQuery = req.query['q'] || '';
-    var options = {
-        protocol: 'https:',
-        hostname: 'api.discogs.com',
-        path: `/database/search?q=${discogsSearchQuery}&token=${process.env.DISCOGS_TOKEN}&type=master`,
-        headers: {
-            'User-Agent': 'Disconest/1.0 +http://www.disconest.com',
-        },
-    };
-    const discogsApiResponse = await makeRequest(options);
-    res.status(200).json(discogsApiResponse);
+    // discogs search
+    if (req.query['q']){
+        let discogsSearchQuery = req.query['q'].toString() || '';
+        const options = {
+            protocol: 'https:',
+            hostname: 'api.discogs.com',
+            path: `/database/search?q=${encodeURIComponent(discogsSearchQuery)}&token=${process.env.DISCOGS_TOKEN}&type=master`,
+            headers: {
+                'User-Agent': 'Disconest/1.0 +http://www.disconest.com',
+            },
+        };
+        const discogsApiResponse = await makeRequest(options);
+        res.status(200).json(discogsApiResponse);
+    }
+    // discogs release
+    if (req.query['release']){
+        let discogsReleaseId = req.query['release'] || '';
+        const options = {
+            protocol: 'https:',
+            hostname: 'api.discogs.com',
+            path: `/releases/${discogsReleaseId}?token=${process.env.DISCOGS_TOKEN}`,
+            headers: {
+                'User-Agent': 'Disconest/1.0 +http://www.disconest.com',
+            },
+        };
+        const discogsApiResponse = await makeRequest(options);
+        res.status(200).json(discogsApiResponse);
+    }    
+    // discogs master
+    if (req.query['master']){
+        let discogsReleaseId = req.query['master'] || '';
+        const options = {
+            protocol: 'https:',
+            hostname: 'api.discogs.com',
+            path: `/masters/${discogsReleaseId}?token=${process.env.DISCOGS_TOKEN}`,
+            headers: {
+                'User-Agent': 'Disconest/1.0 +http://www.disconest.com',
+            },
+        };
+        const discogsApiResponse = await makeRequest(options);
+        res.status(200).json(discogsApiResponse);
+    }
+    res.status(200).json({});
 };
