c50c6cfb989f000aae7120d987246dbee2433b89
Karl Tryggvason <ktryggvason@gmail.com>
2024-03-11 22:26:39 +0200

    Fix discogs link

--- disconest/src/app/page.tsx
+++ disconest/src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
             </div>
             <div id="info" className="row">            
                 <p>
-                    Vinyl sounds better, looks better, feels better and even smells better. But digital does have its benefits, musical metadata is one of them. Having the <em>key</em>, <em>tempo</em> and other musical metadata for your records at a glance would be useful! Disconest uses <a target="_blank" href="http://the.echonest.com/">The Echonest</a> music database to find this information about records and cds registered on <a target="_blank" href="http://www.discogs.com/developers/">Discogs</a>. 
+                    Vinyl sounds better, looks better, feels better and even smells better. But digital does have its benefits, musical metadata is one of them. Having the <em>key</em>, <em>tempo</em> and other musical metadata for your records at a glance would be useful! Disconest uses <a target="_blank" href="http://the.echonest.com/">The Echonest</a> music database to find this information about records and cds registered on <a target="_blank" href="http://www.discogs.com">Discogs</a>. 
                 </p>    
                 <p>    
                     Enter a search term or a Discogs url in the box above to try it out or drag this 
