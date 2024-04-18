69b5fc48474556ac97c4dbc480c6d445c069c659
Karl Tryggvason <ktryggvason@gmail.com>
2024-04-18 15:26:41 +0200

    Take two passes at matching titles

--- src/app/spotify.tsx
+++ src/app/spotify.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { DiscogsTrackProps } from './release';
 import { BPMCounter } from './bpmcounter';
 import { TrackWithAudioFeatures } from './types/spotify';
+import unidecode from 'unidecode';
 
 const keys = ['C', 'C#', 'D', 'E♭', 'E', 'F', 'F#', 'G', 'A♭', 'A', 'B♭', 'B'];
 const mode = ['Minor', 'Major'];
@@ -11,14 +12,41 @@ export const matchDiscogsAndSpotifyTracks = (discogsTracks: DiscogsTrackProps[],
     if (!spotifyTracks){
         return discogsTracks
     };
-    return discogsTracks.map(discogsTrack => {
-        const spotifyTrack = spotifyTracks.find(spotifyTrack => {
+    const timeDurationRegex = /^\d{1,2}:\d{2}$/
+    return discogsTracks.map((discogsTrack, discogsTrackIndex) => {
+        const discogsName = discogsTrack.title.toLowerCase().replace(/[.,()"]/g, '')
+        let spotifyTrack = spotifyTracks.find(spotifyTrack => {
             // replace punctuation, parentheses and case to compare track names
-            const spotifyName = spotifyTrack.name.toLowerCase().replace(/[.,()]/g, '')
-            const discogsName = discogsTrack.title.toLowerCase().replace(/[.,()]/g, '')
+            const spotifyName = spotifyTrack.name.toLowerCase().replace(/[.,()"]/g, '')
             const name = spotifyName.indexOf(discogsName) != -1 || discogsName.indexOf(spotifyName) != -1
-            return name
+            return name;
         })
+        // take another stab at trying to find a match, less precise matching, but might be good enough
+        if (!spotifyTrack) {
+            spotifyTrack = spotifyTracks.find((spotifyTrack, spotifyTrackIndex) => {
+                // compare the ascii versions of both tracks
+                const spotifyName = unidecode(spotifyTrack.name.toLowerCase().replace(/[.,()"]/g, '')).
+                    replace(' & ', ' ').replace(' and ', ' ').trim();
+                // replace certain characters that often differ between the platforms
+                const discogsNameFiltered = unidecode(discogsName).replace(' & ', ' ').replace(' and ', ' ').trim();
+                const name = spotifyName.indexOf(discogsNameFiltered) != -1 || discogsNameFiltered.indexOf(spotifyName) != -1
+                if (name){
+                    return name;
+                }
+                // check for track position and duration match
+                if (discogsTrack.duration && timeDurationRegex.test(discogsTrack.duration)){
+                    const [minutes, seconds] = discogsTrack.duration.split(':').map(num => parseInt(num));
+                    const durationInSeconds = minutes * 60 + seconds;
+                    const index = spotifyTrackIndex === discogsTrackIndex;
+                    // if the index is the same and the duration differs by less than 2 seconds, its probably a match
+                    if (index && Math.abs(durationInSeconds - spotifyTrack.duration_ms/1000) < 2){
+                        return true;
+                    }
+                }
+                return false;
+            })
+        }
+        // if we still haven't found a match, return the original track
         if (!spotifyTrack) {
             return {
                 ...discogsTrack
